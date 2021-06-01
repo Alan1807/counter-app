fe67392f954f61ba5ba357ea8099169549b1b31c
Alan1807 <alannmv@hotmail.com>
2021-06-01 10:30:01 -0800

    main

--- src/tests/base/11-async-await.test.js
+++ src/tests/base/11-async-await.test.js
@@ -5,9 +5,14 @@ describe('Pruebas con async-await y Fetch', () => {
     test('debe de retornar el url de la imagen', async() => {
         
         const url = await getImagen();
-
         expect( url.includes('https://') ).toBe( true );
 
+    });
+
+    test('debe de retornar No existe', async() => {
+
+        const url = await getImagen();
+        expect( url ).toBe('No existe');
     });
 
 });
